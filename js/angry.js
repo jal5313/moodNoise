64ca80d7b4aeadddb7cc84362ad904288771862b
jal5313 <jal5313@rit.edu>
2017-04-27 08:34:52 -0400

    fixing angry to use new system

--- js/angry.js
+++ js/angry.js
@@ -1,7 +1,17 @@
-/*angry*/
-var t;
+/*angry*/    
+var resolution = 360; // how many points in the circle
+var rad = 150;
+var x = 1;
+var y = 1;
 
-function setup() {
+var t = 0; // time passed
+var tChange = .01; // how quick time flies
+
+var nVal; // noise value
+var nInt = 1; // noise intensity
+var nAmp = 1; // noise amplitude
+
+function setup() {    
   createCanvas(window.innerWidth, window.innerHeight);
   background(0);
   stroke(255, 40, 40, 30);
@@ -9,26 +19,25 @@ function setup() {
   noFill();
   t = 0;
 }
-function draw() {
+function draw() {    
   translate(width/2, height/2);
+  nInt = 30; // .1 to 30
+  nAmp = .3; // 0.0 to 1.0
+
   beginShape();
-  for (var i = 0; i < 200; i++) {
-    var ang = map(i, 0, 200, 0, TWO_PI);
-    var rad = 200 * noise(i * 0.1, t * 0.01);
-    var x = rad * cos(ang);
-    var y = rad * sin(ang);
-    curveVertex(x, y);
-  }
+  for (var a=0; a<=TWO_PI; a+=TWO_PI/resolution) {
+
+    nVal = map(noise( cos(a)*nInt+1, sin(a)*nInt+1, t ), 0.0, 1.0, nAmp, 1.0); 
+
+    x = cos(a)*rad *nVal;
+    y = sin(a)*rad *nVal;
+    vertex(x, y);
+    
+    }
   endShape(CLOSE);
 
-  t += .5;
+  t += tChange;
+    
+    
   background(0, 10);
-  // clear the background every 600 frames using mod (%) operator
-
-	
-/*
- if (frameCount % 600 == 0) {
-	background(0);
-  }
-  */
 }
